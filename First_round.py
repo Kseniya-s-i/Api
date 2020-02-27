9aa2fe09cc83e5eac4e30ec1960df2976906f6b9
kseniya <devksuksu@mail.ru>
2020-02-27 16:35:53 +0300

    moving modify

--- First_round.py
+++ First_round.py
@@ -10,7 +10,7 @@ spns = ["0.002", "0.002"]
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-
+clock = pygame.time.Clock()
 pygame.display.flip()
 running = True
 while running:
@@ -20,30 +20,40 @@ while running:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             if -180 < float(coords[0]) + float(spns[0]) < 180:
-                coords[0] = str(float(coords[0]) + float(spns[0]))
+                coords[0] = str(float(coords[0]) + float(spns[0]) / 4)
             # print('r')
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
             if -180 < float(coords[0]) - float(spns[0]) < 180:
-                coords[0] = str(float(coords[0]) - float(spns[0]))
+                coords[0] = str(float(coords[0]) - float(spns[0]) / 4)
             # print('l')
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            if -90 < float(coords[1]) + float(spns[0]) / 2 < 90:
-                coords[1] = str(float(coords[1]) + float(spns[0]) / 2)
-            # print('u')
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            if -90 < float(coords[1]) - float(spns[0]) / 2 < 90:
-                coords[1] = str(float(coords[1]) - float(spns[0]) / 2)
-            # print('d')
+            if -90 < float(coords[1]) + float(spns[0]) / 4 < 90:
+                coords[1] = str(float(coords[1]) + float(spns[0]) / 4)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-            if 0 < float(spns[0]) + float(spns[0]) * 0.5 < 181:
+            if 0.0005 < float(spns[0]) + float(spns[0]) * 0.5 < 181:
                 spns[0] = spns[1] = str(float(spns[0]) + float(spns[0]) * 0.5)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-            if 0 < float(spns[0]) - float(spns[0]) * 0.5 < 181:
+            if 0.0005 < float(spns[0]) - float(spns[0]) * 0.5 < 181:
                 spns[0] = spns[1] = str(float(spns[0]) - float(spns[0]) * 0.5)
 
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            if -90 < float(coords[1]) - float(spns[0]) / 4 < 90:
+                coords[1] = str(float(coords[1]) - float(spns[0]) / 4)
+
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        if -90 < float(coords[1]) - float(spns[0]) / 4 < 90:
+            coords[1] = str(float(coords[1]) - float(spns[0]) / 4)
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        if -90 < float(coords[1]) + float(spns[0]) / 4 < 90:
+            coords[1] = str(float(coords[1]) + float(spns[0]) / 4)
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        if -180 < float(coords[0]) - float(spns[0]) < 180:
+            coords[0] = str(float(coords[0]) - float(spns[0]) / 4)
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        if -180 < float(coords[0]) + float(spns[0]) < 180:
+            coords[0] = str(float(coords[0]) + float(spns[0]) / 4)
     map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}&spn={spns[0]},{spns[1]}&l=map"
     response = requests.get(map_request)
-
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
@@ -55,6 +65,7 @@ while running:
         file.write(response.content)
     screen.fill(pygame.Color('white'))
     screen.blit(pygame.image.load(map_file), (0, 0))
+    clock.tick(90)
     pygame.display.flip()
 pygame.quit()
 
